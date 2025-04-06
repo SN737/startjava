ead2f0892085e95e4c2870555b0f776c934f3b6d
Sergey <92508690+SN737@users.noreply.github.com>
2025-04-07 00:06:12 +0300

    Задание на именование переменны

---
+++ Lesson_1/VariableNamesTheme.java
@@ -0,0 +1,18 @@
+public class VariableNamesTheme{
+    public static void main(String [] args) {
+        System.out.println("1. РАЗНЫЕ ПЕРЕМЕННЫЕ");
+        // цифра
+        int digit = 0;
+        System.out.println("цифра = " + digit);
+
+        // сумма чисел
+        int numSum = 5 + 5;
+        System.out.println("сумма чисел = " + numSum);
+
+        // произведение цифр
+        int digitMultiply = 5 * 5;
+        System.out.println("произведение цифр = " + digitMultiply);
+
+    }
+    
+}
